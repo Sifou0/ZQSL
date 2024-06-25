77d01b9656aead5ba99a64be8c9a18fc90bbf840
Sifou <ismailboumehdi@gmail.com>
2024-06-26 00:14:17 +0200

    methods signature

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Node<T> {
     private static final int MAX_KEYS = 10;
     private int n;
@@ -5,4 +7,23 @@ public class Node<T> {
     Node<T> parent;
     private T[] keys; // SIZE = MAX_KEYS-1
     Node<T>[] children; // SIZE = MAX_KEYS
+
+    private void insert(T element) {}
+
+    private T read(int index) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "n=" + n +
+                ", isLeaf=" + isLeaf +
+                ", parent=" + parent +
+                ", keys=" + Arrays.toString(keys) +
+                ", children=" + Arrays.toString(children) +
+                '}';
+    }
+
 }
+
